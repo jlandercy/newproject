bde7388dc17182495b6497408dfda1946598b35a
jlandercy <jeanlandercy@live.com>
2020-12-28 05:05:24 +0100

    Added Exceptions

--- newproject/errors.py
+++ newproject/errors.py
@@ -3,5 +3,13 @@ Module :py:mod:`newproject.errors` defines all package exceptions.
 """
 
 
+class GenericException(Exception):
+    """
+    Generic Exception, all other exceptions must inherit from it.
+    """
 
 
+class InvalidParameter(GenericException):
+    """
+    Invalid Parameter Exception
+    """
