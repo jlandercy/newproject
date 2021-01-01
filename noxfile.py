189f76e3a84663efccbba1975dbf801496c489ab
jlandercy <jeanlandercy@live.com>
2021-01-01 21:33:00 +0100

    Needs to understand how to pass command

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,15 @@
+import os
 import nox
 
+nox.options.envdir = ".cache"
 
-@nox.session(python=False)
+if os.name == 'nt':
+    nox.options.default_venv_backend = "none"
+
+
+@nox.session
 def pylint(session):
+    """Linter Score"""
     session.install("pylint")
-    session.run("pylint", "newproject")
+    session.run("pylint", "newproject", "--output-format=parseable")
+    #session.run("anybadge", "--value=10", "pylint")
