2593c942e686a40220a43ed6af424b28e519b2aa
jlandercy <jeanlandercy@live.com>
2021-01-01 23:45:48 +0100

    Starting coverage with nox

--- noxfile.py
+++ noxfile.py
@@ -15,22 +15,44 @@ sink = pathlib.Path(nox.options.envdir) / 'reports'
 sink.mkdir(exist_ok=True)
 
 
-re_pylint = re.compile("Your code has been rated at (?P<score>[-.\d]*)/10")
-
-
 @nox.session
 def pylint(session):
     """Linter Score"""
     process = subprocess.run(
         ["pylint", "newproject", "--output-format=parseable"],
         capture_output=True,
-        text=True,
+        encoding='utf-8'
     )
+    session.log(" ".join(process.args))
     session.log(process.stdout)
-    score = float(re_pylint.findall(process.stdout)[0])
+    pattern = re.compile(r"Your code has been rated at (?P<score>[-.\d]*)/10")
+    score = float(pattern.findall(process.stdout)[0])
+    badge = sink / 'pylint.svg'
+    if badge.exists():
+        badge.unlink()
     session.run(
         "anybadge",
         "--value={}".format(score),
-        "--file={}".format(sink / 'pylint.svg'),
+        "--file={}".format(badge),
         "pylint"
     )
+
+
+@nox.session
+def coverage(session):
+    """Coverage Score"""
+    env = {"COVERAGE_FILE": str(sink / ".coverage")}
+    session.run("coverage", "run", "-m", "unittest", "discover", "-v", "newproject.tests", env=env)
+    session.run("coverage", "report", "--omit=venv/**/*", env=env)
+    session.run("coverage", "xml", "-o", str(sink / "coverage.xml"), env=env)
+    # pattern = re.compile(r"Your code has been rated at (?P<score>[-.\d]*)/10")
+    # score = float(pattern.findall(process.stdout)[0])
+    # badge = sink / 'pylint.svg'
+    # if badge.exists():
+    #     badge.unlink()
+    # session.run(
+    #     "anybadge",
+    #     "--value={}".format(score),
+    #     "--file={}".format(badge),
+    #     "pylint"
+    # )
