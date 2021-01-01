cfc5acb89b381d1af1b6ee9f4f7ee489f57fc90a
jlandercy <jeanlandercy@live.com>
2021-01-01 23:19:32 +0100

    Not perfect but starting nox with badges

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,8 @@
+import re
 import os
+import pathlib
+import subprocess
+
 import nox
 
 nox.options.envdir = ".cache"
@@ -7,9 +11,26 @@ if os.name == 'nt':
     nox.options.default_venv_backend = "none"
 
 
+sink = pathlib.Path(nox.options.envdir) / 'reports'
+sink.mkdir(exist_ok=True)
+
+
+re_pylint = re.compile("Your code has been rated at (?P<score>[-.\d]*)/10")
+
+
 @nox.session
 def pylint(session):
     """Linter Score"""
-    session.install("pylint")
-    session.run("pylint", "newproject", "--output-format=parseable")
-    #session.run("anybadge", "--value=10", "pylint")
+    process = subprocess.run(
+        ["pylint", "newproject", "--output-format=parseable"],
+        capture_output=True,
+        text=True,
+    )
+    session.log(process.stdout)
+    score = float(re_pylint.findall(process.stdout)[0])
+    session.run(
+        "anybadge",
+        "--value={}".format(score),
+        "--file={}".format(sink / 'pylint.svg'),
+        "pylint"
+    )
